a709872cf934a2f80f7c5b2a2fc746180cb1d0d2
Artem Shiyanov <shiyanov@gmail.com>
2012-03-11 17:25:24 +0400

    first commit

---
+++ src/main/java/my/example/jsf2_jetty_maven/Visitor.java
@@ -0,0 +1,20 @@
+package my.example.jsf2_jetty_maven;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import java.io.Serializable;
+
+@ManagedBean
+@SessionScoped
+public class Visitor implements Serializable {
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
